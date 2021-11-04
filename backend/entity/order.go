1a87690f3981cd24d166a11600cd6e9578da7f86
Narudee2743 <narudee.joy07@gmail.com>
2021-11-04 13:05:59 +0700

    Create Order

--- backend/entity/order.go
+++ backend/entity/order.go
@@ -6,6 +6,13 @@ import (
 	"gorm.io/gorm"
 )
 
+type Status struct {
+	gorm.Model
+	Statusorder string
+	// 1 status มีได้หลาย order
+	Orders []Order `gorm:"foreignKey:StatusID"`
+}
+
 type Order struct {
 	gorm.Model
 	OrderTime time.Time
@@ -15,20 +22,12 @@ type Order struct {
 
 	// PreorderListID ทำหน้าที่เป็น FK
 	PreorderID *uint
-	Preorder   Preorder
+	Preorder   Preorder `gorm:"references:id"`
 
 	// StatusID ทำหน้าที่เป็น FK
 	StatusID *uint
 	Status   Status `gorm:"references:id"`
 
-	Preorders []Preorder `gorm:"foreignKey:OrderID"`
-	Payment   []Payment  `gorm:"foreignKey:OrderID"`
-	Returns   []Return   `gorm:"foreignKey:OrderID"`
-}
-
-type Status struct {
-	gorm.Model
-	Statusorder string
-	// 1 status มีได้หลาย order
-	Orders []Order `gorm:"foreignKey:StatusID"`
+	Payment []Payment `gorm:"foreignKey:OrderID"`
+	Returns []Return  `gorm:"foreignKey:OrderID"`
 }
